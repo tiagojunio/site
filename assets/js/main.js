1d69c6eea14198e66622f13f87698264df37ad5b
Tiago Junio <tijuniob@gmail.com>
2023-03-01 10:22:09 -0300

    feat: :triangular_flag_on_post: foi inserido a parte de munu moblile e tambem o accordion de skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,37 @@ if (navClose) {
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  //When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+  skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills__content skills__close';
+
+  }
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open'
+
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills)
+})
+
+
 
 
 /*==================== QUALIFICATION TABS ====================*/
@@ -38,7 +66,11 @@ if (navClose) {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+  const scrollY = window.pageYOffset
+}
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
